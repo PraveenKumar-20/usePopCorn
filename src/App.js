586b15a43669d4bf8cfa3591ebe5eede036c0a81
Matpathi Praveen Kumar <praveenkumar1214241@gmail.com>
2024-05-27 15:41:20 +0530

    Storing the watched movies in local storage

--- src/App.js
+++ src/App.js
@@ -53,10 +53,16 @@ const KEY = "95b9a9bd";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storeValue = localStorage.getItem("watched");
+    return JSON.parse(storeValue);
+  });
 
   // const tempQuery = "interstellar";
 
@@ -75,6 +81,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
